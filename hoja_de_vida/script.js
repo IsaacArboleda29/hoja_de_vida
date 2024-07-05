3c6fb9352532739e2aeda7c5f0204668d3daea63
IsaacArboleda29 <naranjojaves1004@gmail.com>
2024-07-04 21:51:22 -0700

    act

--- hoja_de_vida/script.js
+++ hoja_de_vida/script.js
@@ -1,4 +1,13 @@
-const bot = document.getElementById('bot');
-bot.addEventListener('click',() => {
-    window.location.href = 'estudios/estudios.html';
-});
+
+function irADatos(){
+    location.href="index.html"
+}
+function irAEstudios(){
+   location.href="itens/estudios.html"
+}
+function irACursos(){
+   location.href="itens/Cursos.html"
+}
+function irAExperiencia(){
+   location.href="itens/Experiencia.html"
+}
