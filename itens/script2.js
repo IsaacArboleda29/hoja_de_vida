d2e192f842d0d8a03cb2e00526f52fb9ba32ca64
IsaacArboleda29 <naranjojaves1004@gmail.com>
2024-07-07 16:20:59 -0700

    Add files via upload

---
+++ itens/script2.js
@@ -0,0 +1,12 @@
+function irADatos(){
+    location.href="../index.html"
+}
+function irAEstudios(){
+    location.href="estudios.html"
+ }
+ function irACursos(){
+    location.href="Cursos.html"
+ }
+ function irAExperiencia(){
+    location.href="Experiencia.html"
+ }
